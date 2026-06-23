bf983fb80b78ec8639283af44f50133f11106cfa
Concretar Concreto Usinado LTDA <pereiralevi115@gmail.com>
2026-06-23 19:53:37 +0000

    Automação de eventos fiscais

--- src/components/invoices/InvoiceDetailDialog.jsx
+++ src/components/invoices/InvoiceDetailDialog.jsx
@@ -9,7 +9,6 @@ import { CheckCircle2, X, Download, Loader2 } from "lucide-react";
 import { base44 } from "@/api/base44Client";
 import { toast } from "sonner";
 import InvoiceStatusBadge from "./InvoiceStatusBadge";
-import FiscalEventsSection from "./FiscalEventsSection";
 import { formatCNPJ } from "@/lib/formatters";
 import { useAuth } from "@/lib/AuthContext";
 
@@ -288,9 +287,6 @@ export default function InvoiceDetailDialog({ invoice: invoiceProp, open, onClos
               </div>
             </div>
           )}
-
-          {/* EVENTOS DO DOCUMENTO */}
-          <FiscalEventsSection events={invoice.fiscal_events} />
 
           {/* CHAVE DE ACESSO */}
           {invoice.access_key && (
